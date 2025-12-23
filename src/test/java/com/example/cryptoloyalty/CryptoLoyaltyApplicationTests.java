d413417745bb66352954838ca8c04b4aa59e6cce
srinadh_p <srinadh.p@optculture.com>
2025-12-23 10:53:42 +0530

    first commit

---
+++ src/test/java/com/example/cryptoloyalty/CryptoLoyaltyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptoloyalty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoLoyaltyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
